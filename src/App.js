7e0bbfcf3da5665d3560423b7136952a406da40a
figerandlmj <2270200373@qq.com>
2018-05-27 10:39:56 +0800

    update

--- src/App.js
+++ src/App.js
@@ -1,16 +1,10 @@
-import React, {
-  Component
-} from 'react';
-import {
-  BrowserRouter,
-  Route,
-  Switch,
-  Redirect
-} from 'react-router-dom';
+import React, { Component } from 'react';
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 import Home from './routes/home/Index';
-import logo from './logo.svg';
+import User from './routes/user/Index';
+import Detail from './routes/detail/Index';
+import Seat from './routes/seat/Index';
 import './App.css';
-
 
 
 class App extends Component {
@@ -19,6 +13,9 @@ class App extends Component {
       <BrowserRouter>
         <Switch>
           <Route path="/" exact component={Home} />
+          <Route path="/user" exact component={User} />
+          <Route path="/detail" exact component={Detail} />
+          <Route path="/seat" exact component={Seat} />
         </Switch>
       </BrowserRouter>
     );
